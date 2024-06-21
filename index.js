3d2bee82fea65345719aa02011c892c3f823afb4
Saurav Tiwari <116860218+saurav-tiwari03@users.noreply.github.com>
2024-06-21 20:20:29 +0530

    ejs

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
 const {dbConnect} = require('./config/database')
 require('dotenv').config()
 
 app.use(express.json());
-app.use(cors())
+app.use(cors()) 
+app.set('view engine', 'ejs')
+app.set('views',path.resolve('./views'))
 
 const PORT = process.env.PORT || 5000;
 
@@ -14,3 +17,6 @@ app.listen(PORT , () => {
   dbConnect();
 })
 
+app.get('/',(req,res) => {
+  return res.render('home')
+})
